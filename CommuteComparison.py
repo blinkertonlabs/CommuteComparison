8d211fb5feb5d82f89e7a476fb074f019866e769
BlinkertonLabs <blinkerton.labs@gmail.com>
2023-07-12 21:05:35 -0400

    Initial commit of the Python script.  Basis for the class and a simple print command.

---
+++ CommuteComparison.py
@@ -0,0 +1,48 @@
+#   Blinkerton Labs
+#   The intent of this Python script is to learn about classes
+#   and complete a what-if analysis that compares different commutes
+#   based on a number of different factors.  This is really just an
+#   excuse to learn more Python with a particular goal in mind.
+
+
+class Commute:
+    def __init__(self, 
+           commuteName,
+           oneWayTripMiles,
+           oneWayTripTimeMinutes,
+           carFuelEconomyMPG,
+           gasCostPerGallon,
+           tripsPerWeek):
+        self.commuteName = commuteName
+        self.oneWayTripMiles = oneWayTripMiles
+        self.oneWatTripTimeMinutes = oneWayTripTimeMinutes
+        self.carFuelEconomyMPG = carFuelEconomyMPG
+        self.gasCostPerGallon = gasCostPerGallon
+        self.tripsPerWeek = tripsPerWeek
+    
+        #   Other parameters that are calculated automatically
+        self.costPerTrip = ((self.oneWayTripMiles * 2) / self.carFuelEconomyMPG) * self.gasCostPerGallon
+        
+        #   Weekly totals
+        self.totalCostPerWeek = self.costPerTrip * self.tripsPerWeek
+        self.totalMilesPerWeek = (self.oneWayTripMiles * 2) * self.tripsPerWeek
+        self.totalTimePerWeekHours = ((self.oneWatTripTimeMinutes * 2) / 60) * self.tripsPerWeek
+
+        #   Annual Totals
+        self.totalCostPerYear = 52 * self.totalCostPerWeek
+        self.totalMilesPerYear = 52 * self.totalMilesPerWeek
+        self.totalTimePerYearDays = (52 * self.totalTimePerWeekHours) / 24
+
+    def __str__(self):
+        return 'Total Cost per week: ' + str(self.totalCostPerWeek)
+
+
+
+
+#   Create the commute scenarios
+
+baseLine = Commute('Baseline', 20, 21, 22, 4.00, 1)
+
+print(baseLine)
+
+
